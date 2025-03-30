620fd38775e473e2a40d9ad657c7d95452ca2ee0
sosaska777 <dima.semnov.2007@mail.ru>
2025-03-30 15:32:35 +1100

    еще не все
    сча еще поделаю

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,18 +1,21 @@
-// src/Button.tsx
+// src/Button/Button.tsx
 import React from 'react';
-import './Button.css';
+
 
 interface ButtonProps {
-  text: string;
-  onClick?: () => void;
+    text: string;
+    onClick?: () => void;
+    disabled?: boolean; // Добавляем пропс для disabled состояния
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
-  return (
-    <button className="rounded-button button-font" onClick={onClick}>
-      {text}
-    </button>
-  );
+const Button: React.FC<ButtonProps> = ({ text, onClick, disabled }) => {
+    return (
+        <div className="button-wrapper"> {/* Оборачиваем кнопку в div с классом button-wrapper */}
+            <button className="button" onClick={onClick} disabled={disabled}>
+                {text}
+            </button>
+        </div>
+    );
 };
 
 export default Button;
