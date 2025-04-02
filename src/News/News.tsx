621b6a4194ef8bceff4c1f577b7b21a1deea7a82
Dmitriy228335 <205258286+sosaska777@users.noreply.github.com>
2025-04-02 17:15:36 +1100

    теперь точно все
    ывафвыпкрыаиавпокно

--- src/News/News.tsx
+++ src/News/News.tsx
@@ -1,7 +1,7 @@
 ﻿// src/News/News.tsx
 import React from 'react';
 import './News.css';
-import './News-button.css'; // Убедитесь, что импортирован CSS для кнопки (ПОСЛЕ News.css)
+import './Newbut.css'
 import Button from '../Button/Button';
 import logo from '../logo.png'; // Импортируем изображение
 
